43a634439310f2f2d077b5b7d377b3bf0dc1bf97
ultimatefaloe <ultimatefaloe@outlool.com>
2025-11-21 19:20:33 +0100

    whatsapp structure

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// 2 value is need for the function to calculate or add
+// add variables to give result
+// and result is declare as variable 
+// it return result
+function addNumber (x, y){
+  let result = x + y;
+  return result;
+}
